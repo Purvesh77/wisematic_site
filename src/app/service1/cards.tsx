6fad82629b66d3f471052ceecde693e75ae612f6
Kashish Meena <202151073@iiitvadodara.ac.in>
2025-01-23 15:54:37 +0530

    Add remaining project files

--- src/app/service1/cards.tsx
+++ src/app/service1/cards.tsx
@@ -18,18 +18,18 @@ const Cards = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8"
                 style={{paddingLeft: '100px', paddingRight: '100px'}}>
                   {/* Card 1: Program and Project Management */}
-                  <div className="bg-white rounded-lg shadow-lg p-8 text-center :">
+                  <div className="bg-white rounded-lg shadow-lg p-8 text-center h-[300px] ">
                   
                       <FaTasks className="text-4xl text-black items-start justify-between" />
                     
-                    <h3 className="text-2xl text-black font-semibold mb-4  "
+                    <h3 className="text-2xl text-black font-semibold mb-4 -ml-10"
                     style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 700 }}>Program and Project Management</h3>
                     <p className="text-base text-gray-700 mb-4"
                     style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 500 }}>Our seasoned project managers blend business acumen with technical expertise. We tailor our approach to your project, whether it’s waterfall, agile, or a hybrid model. Our focus on planning, execution, and stakeholder satisfaction ensures successful delivery every time.</p>
                   </div>
       
                   {/* Card 2: Business Requirements Elicitation and Analysis */}
-                  <div className="bg-white rounded-lg shadow-lg p-8 text-center">
+                  <div className="bg-white rounded-lg shadow-lg p-8 text-center h-[300px]">
                   
                       <FaChartLine className="text-4xl text-black" />
                    
@@ -40,22 +40,22 @@ const Cards = () => {
                   </div>
       
                   {/* Card 3: Business Process Automation */}
-                  <div className="bg-white rounded-lg shadow-lg p-8 text-center">
+                  <div className="bg-white rounded-lg shadow-lg p-8 text-center h-[300px]">
                   
                       <FaRobot className="text-4xl text-black" />
                    
-                    <h3 className="text-2xl text-black font-semibold mb-4"
+                    <h3 className="text-2xl text-black font-semibold mb-4 -ml-20"
                     style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 700 }}>Business Process Automation</h3>
                     <p className="text-base text-gray-700 mb-4"
                     style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 500 }}>We help our clients to build faster, leaner, and more digitized businesses with automated business workflows. Our teams define the implementation strategy, transition plan, and the new governance structure.</p>
                   </div>
       
                   {/* Card 4: Technology Integration Strategy */}
-                  <div className="bg-white rounded-lg shadow-lg p-8 text-center">
+                  <div className="bg-white rounded-lg shadow-lg p-8 text-center h-[300px]">
                   
                       <FaCogs className="text-4xl text-black" />
                     
-                    <h3 className="text-2xl text-black font-semibold mb-4"
+                    <h3 className="text-2xl text-black font-semibold mb-4 -ml-20"
                     style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 700 }}>Technology Integration Strategy</h3>
                     <p className="text-base text-gray-700 mb-4"
                     style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 500 }}>Our expert Solution Architect will first understand our clients’ objectives, review their environments, system capabilities, and technology components, and then map out an integration strategy that defines efficient interfaces and connection protocols among in-scope systems.</p>
@@ -64,7 +64,7 @@ const Cards = () => {
                   {/* Rodmap Card */}
                  
                   <div className="bg-white shadow-lg rounded-lg  p-8 text-center col-span-2 mt-20 ">
-        <div className="flex items-center justify-center w-[75px] h-[75px] mb-4 mx-auto ">
+        <div className="flex items-center justify-center w-[75px] h-[80px] mb-4 mx-auto ">
           <FaRoute className="text-4xl text-black" />
         </div>
         
